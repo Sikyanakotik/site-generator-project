f206aaa2db9f3d4a07c9d719c413cc452a422d8f
Sikyanakotik <patrick_reding@hotmail.com>
2025-12-24 20:41:16 -0500

    CH5L2

--- src/main.py
+++ src/main.py
@@ -3,8 +3,29 @@ import shutil
 
 from textnode import TextNode, TextType
 
+PUBLIC_PATH = "public/"
+STATIC_PATH = "static/"
+
 def copy_static_to_public():
-    shutil.rmtree("/public")
+    def copy_dir_to_dir(source_path, dest_path):
+        source_contents = os.scandir(source_path)
+        for entry in source_contents:
+            dest = os.path.join(dest_path, entry.name)
+            if entry.is_dir():
+                os.mkdir(dest)
+                copy_dir_to_dir(entry.path, dest)
+            else:
+                shutil.copy(entry, dest)
+    #----
+
+    if os.path.exists(PUBLIC_PATH):
+        shutil.rmtree(PUBLIC_PATH)
+    os.mkdir(PUBLIC_PATH)
+    
+    copy_dir_to_dir(STATIC_PATH, PUBLIC_PATH)
+    
+
+    
 
 
 def main():
